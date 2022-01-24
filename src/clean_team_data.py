61db09e49bbafdc7db14ffc4de8100915fb99b30
gargparthb <62770333+gargparthb@users.noreply.github.com>
2022-01-24 16:42:12 -0500

    data cleaning notebook finished

--- src/clean_team_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv('../data/teams.csv')
-filtered = data[['Team', 'W', 'L', 'PW', 'PL', 'MOV', 'SOS', 'SRS', 'ORtg', 'DRtg', 'NRtg', 'Pace',
-'FTr', '3PAr', 'TS%', "eFG%", 'TOV%', 'ORB%', "FT/FGA", "Arena", "Attend.", "Attend./G"]]
-
-filtered.to_csv("../data/processed_teams.csv")
