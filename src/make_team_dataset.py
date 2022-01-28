53641f8ba7c73357eee00bf9abdabe8bcea05b4d
gargparthb <62770333+gargparthb@users.noreply.github.com>
2022-01-28 15:40:45 -0500

    normalization

--- src/make_team_dataset.py
+++ src/make_team_dataset.py
@@ -8,7 +8,7 @@ for year in range(1977, 2021):
     url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + ".html"
     page = requests.get(url)
 
-	# selects the table from the webpage		
+  # selects the table from the webpage		
     soup = BeautifulSoup(page.content, "html.parser")
     table = soup.find(
         lambda tag: tag.name == "table"
@@ -19,17 +19,17 @@ for year in range(1977, 2021):
     table_head = table.find("thead").findAll("tr")[1]
     table_body = table.find("tbody").findAll("tr")
 
-	# extracts data from table head
+  # extracts data from table head
     column_headers = [th.getText() for th in table_head]
     filtered_column_headers = list(filter(lambda t: t != " ", column_headers))[1:]
 
-	# actual data from the table
+  # actual data from the table
     team_data = [
         [td.getText() for td in table_body[i].findAll("td")]
         for i in range(len(table_body))
     ]
 
-	# integration with the master dataframe
+  # integration with the master dataframe
     df = pd.DataFrame(team_data, columns=filtered_column_headers)
     df["Year"] = year
     data = pd.concat([data, df])
